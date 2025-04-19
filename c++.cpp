eb9bd8361218d246092f4ba3a311e6612d5a9ab8
Deeh878 <dainesskato@gmail.com>
2025-04-19 22:26:37 +0300

    Update c++.cpp

--- c++.cpp
+++ c++.cpp
@@ -76,7 +76,7 @@ public:
             getline(cin, title);
             cout << "Enter book author: ";
             getline(cin, tittle);
-            library.addBook(tit)
+            library.addBook(title)
         
     
         
